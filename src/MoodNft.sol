14ae6d962d5c0994d00e83d7ff04a5cda0f4ca6c
luiscaceresd <luiscaceresd97@gmail.com>
2024-02-28 19:31:11 -0400

    adding happy and sad svgs

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -3,15 +3,23 @@
 pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
-    string private s_sadSvg;
-    string private s_happySvg;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
+
+    enum Mood {
+        HAPPY, 
+        SAD
+    }
+
+    mapping(uint256 => MOOD) private s_tokenIdToMood;
 
     constructor(
-        string memory sadSvg,
-        string memory happySvg
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
     ) ERC721("Mood NFT", "MN") {
         s_tokenCounter = 0;
         s_sadSvg = sadSvg;
@@ -20,10 +28,18 @@ contract MoodNft is ERC721 {
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
     }
 
     function tokenURI(uint256 tokenId) public view override returns(string memory) {
+        string memory imageURI;
+        if(s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageURI = s_happySvgImageUri;
+        } else {
+            imageURI = sadSvgImageUri;
+        }
 
+        string memory tokenMetadata = string.concat('{"name": "', name(), '", "description": "An NFT that reflects the owners mood.", "atributes": [{"trait_type": "moodiness", "value": 100}], "image": "', imageURI, '"}')
     }
 }
