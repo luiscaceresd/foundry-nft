b8963da9307ee07fc4ff286e3336987a1f973bdb
luiscaceresd <luiscaceresd97@gmail.com>
2024-02-28 20:13:43 -0400

    adding mint NFT for dynamic nfts

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -15,15 +15,15 @@ contract MoodNft is ERC721 {
         SAD
     }
 
-    mapping(uint256 => MOOD) private s_tokenIdToMood;
+    mapping(uint256 => Mood) private s_tokenIdToMood;
 
     constructor(
         string memory sadSvgImageUri,
         string memory happySvgImageUri
     ) ERC721("Mood NFT", "MN") {
         s_tokenCounter = 0;
-        s_sadSvg = sadSvg;
-        s_happySvg = happySvg;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
     }
 
     function mintNft() public {
@@ -32,14 +32,31 @@ contract MoodNft is ERC721 {
         s_tokenCounter++;
     }
 
+    function _baseURI() internal pure override returns(string memory) {
+        return "data:application/json;base64,";
+    }
+
     function tokenURI(uint256 tokenId) public view override returns(string memory) {
         string memory imageURI;
         if(s_tokenIdToMood[tokenId] == Mood.HAPPY) {
             imageURI = s_happySvgImageUri;
         } else {
-            imageURI = sadSvgImageUri;
+            imageURI = s_sadSvgImageUri;
         }
+        
 
-        string memory tokenMetadata = string.concat('{"name": "', name(), '", "description": "An NFT that reflects the owners mood.", "atributes": [{"trait_type": "moodiness", "value": 100}], "image": "', imageURI, '"}')
+        return 
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name": "', name(), '", "description": "An NFT that reflects the owners mood.", "atributes": [{"trait_type": "moodiness", "value": 100}], "image": "', imageURI, '"}'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
